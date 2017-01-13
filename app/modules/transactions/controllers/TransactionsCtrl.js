26b1473daf0af6c6941e529d34d6f31b8680db07
Geraldine Starke <geraldine@starke.fr>
2017-01-13 16:24:10 +0100

    feat(transactions): multiselection + detail view for single and multiple items + pagination

--- app/modules/transactions/controllers/TransactionsCtrl.js
+++ app/modules/transactions/controllers/TransactionsCtrl.js
@@ -1,3 +1,4 @@
+/* globals window */
 (function () {
   'use strict';
 
@@ -7,15 +8,15 @@
   ;
 
   function TransactionsCtrl(API, $location, $q, $timeout) {
-
     var vm = this;
+
     vm.transactions = [];
     vm.filter = {
       options: {
         debounce: 500
       }
     };
-    vm.limitOptions = [50, 100, 200, {
+    vm.limitOptions = [10, {
       label: 'All',
       value: function () {
         return vm.transactions ? vm.transactions.length : 0;
@@ -23,24 +24,59 @@
     }];
     vm.query = {
       order: 'nameToLower',
-      limit: 100,
+      limit: 10,
       page: 1,
       filter: '',
-      count: 0
+      totalcount: 0
     };
+    vm.selected = [];
 
-    vm.getTransactions = function () {
-      return API.Swagger.getTransactions()
+    vm.getTransactions = function (page, limit) {
+      var _page = page || vm.query.page;
+      var _limit = limit || vm.query.limit;
+      return API.Swagger.getTransactions({
+        page: parseInt(_page),
+        pagesize: parseInt(_limit)
+      })
         .then(function (res) {
+          console.log(res);
           vm.transactions = res.transactions;
+          vm.query.totalcount = res.meta.total_count;
+          vm.query.totalpages = res.meta.total_pages;
         })
         .catch(function (err) {
           console.log(err);
         })
       ;
     };
+    vm.toggleLimitOptions = function () {
+      vm.limitOptions = vm.limitOptions ? undefined : [10];
+    };
+    vm.onPaginate = function (page, limit) {
+      vm.selected = [];
+      var promise = vm.getTransactions(page, limit);
+      return promise;
+    };
     vm.loadStuff = function () {
       vm.promise = $timeout(function () {
+      }, 2000);
+    };
+    vm.onReorder = function (order) {
+      /* eslint-disable no-console */
+      console.log(`Scope Order: ${vm.query.order}`);
+      console.log(`Order: ${order}`);
+      /* eslint-enable no-console */
+      vm.promise = $timeout(function () {
+        vm.orders.data.sort(function (a, b) {
+          if (a.name > b.name) {
+            return 1;
+          }
+          if (a.name < b.name) {
+            return -1;
+          }
+          // a is equal to b
+          return 0;
+        });
       }, 2000);
     };
 
